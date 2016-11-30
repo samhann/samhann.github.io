1dba3c513dfa682954e2cdb64dd1c813600f29db
Samhan Salahuddin <samhan@ABC-XYZ.local>
2016-11-30 20:51:32 +0530

    Expr

---
+++ casso/c.js
@@ -0,0 +1,26 @@
+/**
+ * Parts Copyright (C) 2011-2012, Alex Russell (slightlyoff@chromium.org)
+ * Parts Copyright (C) Copyright (C) 1998-2000 Greg J. Badros
+ *
+ * Use of this source code is governed by http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This is a compiled version of Cassowary/JS. For source versions or to
+ * contribute, see the github project:
+ *
+ *  https://github.com/slightlyoff/cassowary-js-refactor
+ *
+ */
+
+(function() {
+<html>
+<head><title>301 Moved Permanently</title></head>
+<body bgcolor="white">
+<center><h1>301 Moved Permanently</h1></center>
+<hr><center>nginx/1.6.2</center>
+</body>
+</html>
+
+}).call(
+  (typeof module != "undefined") ?
+      (module.compiled = true && module) : this
+);
